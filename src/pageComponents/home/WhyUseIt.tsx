1824b1d9e42a9db7db672f2a626bebc2501264f5
Jon Grinnell <jongrinnell@Jons-MacBook-Pro.local>
2024-06-04 12:27:56 -0700

    base tee m

--- src/pageComponents/home/WhyUseIt.tsx
+++ src/pageComponents/home/WhyUseIt.tsx
@@ -18,12 +18,9 @@ const products = [
   { name: "Socks (Unisex)", price: 5, imageUrl: '/socks.png' },
 
   { name: "Onchain summer sunglasses", price: 5, imageUrl: "https://www.lockerroomsportsapparel.com/cdn/shop/products/c24.jpg?v=1681920691" },
-  
   { name: "Bit by Bit Childrens Book", price: 25, imageUrl: '/book.png' },
   { name: "BTC Tee: Medium (Womens)", price: 5, imageUrl: '/btc.jpeg' },
-
   { name: "BTC Tee: Large (Mens)", price: 5, imageUrl: '/btc.jpeg' },
-
   { name: "Coin Tee: XS", price: 10, imageUrl: "https://coinbaseshop.com/cdn/shop/files/20240203_845a_Photoshoot_Coinbase-Merch-Q1_IMGP9600_b4e81208-fd06-4205-b0d8-739bcaa527ac.jpg?v=1707459407" },
   { name: "Coin Tee: Small", price: 10, imageUrl: "https://coinbaseshop.com/cdn/shop/files/20240203_845a_Photoshoot_Coinbase-Merch-Q1_IMGP9600_b4e81208-fd06-4205-b0d8-739bcaa527ac.jpg?v=1707459407" },
   { name: "Coin Tee: Medium", price: 10, imageUrl: "https://coinbaseshop.com/cdn/shop/files/20240203_845a_Photoshoot_Coinbase-Merch-Q1_IMGP9600_b4e81208-fd06-4205-b0d8-739bcaa527ac.jpg?v=1707459407" },
@@ -33,7 +30,7 @@ const products = [
 
   { name: "Base Tee: XS", price: 15, imageUrl: '/baseT.png' },
   { name: "Base Tee: Small", price: 15, imageUrl: '/baseT.png' },
-  { name: "Base Tee: Medium", price: 15, imageUrl: '/baseT.png' },
+  // { name: "Base Tee: Medium", price: 15, imageUrl: '/baseT.png' },
   { name: "Base Tee: Large", price: 15, imageUrl: '/baseT.png' },
   { name: "Base Tee: XL", price: 15, imageUrl: '/baseT.png' },
   { name: "Base Tee: 2XL", price: 15, imageUrl: '/baseT.png' },
