6dfeae62d8609fcfc5da83ddb8b3b684f84a7759
Jon <jgrinnell0@gmail.com>
2024-07-17 11:11:41 -0700

    base tees

--- src/pageComponents/home/WhyUseIt.tsx
+++ src/pageComponents/home/WhyUseIt.tsx
@@ -32,8 +32,8 @@ const products = [
   // { name: "Base Tee: Small", price: 15, imageUrl: '/baseT.png' },
   // { name: "Base Tee: Medium", price: 15, imageUrl: '/baseT.png' },
   // { name: "Base Tee: Large", price: 15, imageUrl: '/baseT.png' },
-  // { name: "Base Tee: XL", price: 15, imageUrl: '/baseT.png' },
-  // { name: "Base Tee: 2XL", price: 15, imageUrl: '/baseT.png' },
+  { name: "Base Tee: XL ONLY", price: 15, imageUrl: '/baseT.png' },
+  { name: "Base Tee: 2XL ONLY", price: 15, imageUrl: '/baseT.png' },
 
   // { name: "Shadowy Super Coder Hoodie: Extra Small", price: 25, imageUrl: "https://coinbaseshop.com/cdn/shop/files/20240203_845a_Photoshoot_Coinbase-Merch-Q1_IMGP9694.jpg?v=1707459485" },
   // { name: "Shadowy Super Coder Hoodie: Small", price: 25, imageUrl: "https://coinbaseshop.com/cdn/shop/files/20240203_845a_Photoshoot_Coinbase-Merch-Q1_IMGP9694.jpg?v=1707459485" },
